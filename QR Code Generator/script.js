6b060493bc352050169a668d6f1c19cd8a8e3c1f
Dhruv Dipak Pawar <89768718+DHRUVPAWAR12@users.noreply.github.com>
2024-04-24 15:16:03 +0530

    Update script.js

--- QR Code Generator/script.js
+++ QR Code Generator/script.js
@@ -1,1 +1,17 @@
+const imgBox = document.getElementById( "imgbox");
+const qrimg = document.getElementById("qrimage");
+const qrText = document.getElementById("qrText");
+
+function generateQR(){
+if(qrText.value.length > 0){
+qrimg.src ="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ qrText.value;
+imgBox.classList.add("show-img");
+}else{
+    qrText.classList.add('error');
+    setTimeout(()=>{
+        qrText.classList.remove('error');
+    },1000);
+}
+
+}
 
