16c656fae924ad59337a944bd48cb580577be94d
Dhruv Dipak Pawar <89768718+DHRUVPAWAR12@users.noreply.github.com>
2024-04-26 17:03:59 +0530

    Update script.js

--- Image background remover/script.js
+++ Image background remover/script.js
@@ -1,1 +1,14 @@
+var imgBox = document.querySelector(".imgBox");
+var imgWrap =document.querySelector(".img-wrap");
+var  originalImg = document.getElementById("originalImg");
+var line = document.getElementById("line");
+originalImg.style.width =imgBox.offsetWidth + "px";
+var leftspace = imgBox.offsetLeft;
+
+imgBox.onmousemove = function(e){
+        var boxWidth = (e.pageX - leftspace) + "px" ;
+        imgWrap.style.width = boxWidth;
+        line.style.left = boxWidth ;
+}
+
 
