2cd9d7f1fdfd09a27e0ba9a641802d1348aa1e4f
Dhruv Dipak Pawar <89768718+DHRUVPAWAR12@users.noreply.github.com>
2024-04-30 11:11:05 +0530

    Update script.js

--- Digital  Clock/script.js
+++ Digital  Clock/script.js
@@ -1,1 +1,14 @@
 
+
+const hrs = document.getElementById("hrs");
+const mins = document.getElementById("mins");
+const secs = document.getElementById("secs");
+
+setInterval(()=>{
+let currentTime = new  Date();
+
+hrs.innerHTML = (currentTime.getHours()< 10 ? "0" : "") + currentTime.getHours();
+mins.innerHTML = (currentTime.getMinutes()< 10 ? "0" : "" )+ currentTime.getMinutes();
+secs.innerHTML= (currentTime.getSeconds()< 10 ? "0" : "" ) + currentTime.getSeconds();
+},1000)
+
