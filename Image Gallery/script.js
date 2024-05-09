385249875a5aca7efa558fc817dc37a4c18b1e8f
Dhruv Dipak Pawar <89768718+DHRUVPAWAR12@users.noreply.github.com>
2024-05-09 15:26:10 +0530

    Update script.js

--- Image Gallery/script.js
+++ Image Gallery/script.js
@@ -1,1 +1,17 @@
+let scrollContainer = document.getElementById(".gallery");
+let backbtn = document.getElementById("leftbtn");
+let nextbtn = document.getElementById("rightbtn");
 
+scrollContainer.addEventListener("wheel" ,(evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehavior = "auto";
+});
+nextbtn.addEventListener("click", ()=>{
+    scrollContainer.scrollLeft += 900;
+    scrollContainer.style.scrollBehavior = "smooth";
+});
+backbtn.addEventListener("click", ()=>{
+    scrollContainer.scrollright -= 900;
+    scrollContainer.style.scrollBehavior = "smooth";
+});
