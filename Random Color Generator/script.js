6c1a1951a1fe121453905bf93117d83f4a2eb466
Dhruv Dipak Pawar <89768718+DHRUVPAWAR12@users.noreply.github.com>
2024-05-10 16:44:52 +0530

    Update script.js

--- Random Color Generator/script.js
+++ Random Color Generator/script.js
@@ -1,1 +1,10 @@
-
+const getColor = () => {
+    const randomNumber = Math.floor(Math.random() * 16777215);
+    const randomCode = "#" + randomNumber.toString(16);
+    document.body.style.backgroundColor = randomCode;
+    document.getElementById("color-code").innerText = randomCode;
+    //copy the code
+    navigator.clipboard.writeText(randomCode);
+}
+document.getElementById('btn').addEventListener('click',getColor);
+getColor();
